2d071a9498ffb3eaa909689002eaba605e3b8d7f
Luis Mayta <luis.mayta@gmail.com>
2019-01-17 19:36:34 -0500

    Limpiando el proyecto

--- storage/cms/cache/b8/47/404.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5c40f94b4391e123331848_6b05cb09fe669361af270f2d96d0a0d0Class extends Cms\Classes\PageCode
-{
-
-}
--- storage/cms/cache/d4/95/header.htm.php
+++
@@ -1,5 +0,0 @@
-<?php 
-class Cms5c40f94b49aa5141465226_0a37242a9b9dd7e111b385e8f4b47c03Class extends Cms\Classes\PartialCode
-{
-
-}
